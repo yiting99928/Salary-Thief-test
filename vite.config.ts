0641a403ead942fab7cd17de1da07fc46bfb2a55
yumi.yang <yumiyang@synology.com>
2025-12-12 15:59:44 +0800

    Fix vite base URL to match repository name

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Salary-Thief-Calculator/',
+  base: '/Salary-Thief-test/',
   plugins: [react()],
   build: {
     outDir: 'dist',
